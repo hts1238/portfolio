35904faf83f20e7f902e1c2ef7038f3b7432ae0a
Timur <hts1238@mail.ru>
2019-10-03 13:09:03 +0500

    Hide empty pages

--- about.php
+++ about.php
@@ -3,5 +3,6 @@
     include_once("paths.php");
 	include_once(TEMPLATES_FOLDER."header.html");
 	include_once(TEMPLATES_FOLDER."theme_colors.html");
+	include_once(TEMPLATES_FOLDER."about.html");
 	include_once(TEMPLATES_FOLDER."footer.php");
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-	header("Location: allworks");
+	header("Location: about");
 ?>
